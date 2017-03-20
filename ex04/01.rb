880b98f15501fe7c2947eede5eea543197cb5979
Unai Motriko <unaimotriko@gmail.com>
2017-03-20 10:03:33 +0000

    Exercise 1 first file.

---
+++ ex04/01.rb
@@ -0,0 +1,19 @@
+# In this exercise we are looking at variables
+
+# Unlike in math, the "=" here is an assigment operator
+cars = 100 
+space_in_a_car = 4.0 
+drivers = 30
+passengers = 90
+# We can use the variable names instead of their values to compute.
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+puts "There are #{cars} cars available."
+puts "There are only #{drivers} drivers available."
+puts "There will be #{cars_not_driven} empty cars today."
+puts "We can transport #{carpool_capacity} people today."
+puts "We have #{passengers} to carpool today."
+puts "We need to put about #{average_passengers_per_car} in each car."
