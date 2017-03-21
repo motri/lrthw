7cebc312fb77e53cd9757bffca043da62713ba26
Unai Motriko <unaimotriko@gmail.com>
2017-03-21 19:19:33 +0000

    Drills refactoring.

--- ex13/drills01_13.rb
+++ ex13/drills01_13.rb
@@ -1,11 +1,13 @@
 race, type, special_ability, clan = ARGV
 
-puts "Welcome fellow #{race}!"
-puts "I can sense that you will become a master #{type}."
-puts "What is this power?...#{special_ability}... That is most impresive young one."
-
-puts "Before you go forth, what shall we call you? "
+puts """Welcome fellow #{race}!
+I can sense that you will become a master #{type}.
+What is this power?...#{special_ability}...
+That is most impresive young one.
+Before you go forth, what shall we call you?
+"""
 name = $stdin.gets.chomp
-puts "May the Dark Mothers look kindly upon you #{name}."
-
-puts "Join your bretheren at the #{clan} Hall."
+puts """May the Dark Mothers look kindly upon you #{name}.
+Join your bretheren at the #{clan} Hall!
+"""
+# ex.: Elf, Warlock, Fire_Balls, Red_Frost
