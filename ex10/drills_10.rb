f02b48f518558286624d353b338ae8d0343fbb34
Unai Motriko <unaimotriko@gmail.com>
2017-03-21 10:55:20 +0000

    Exercise 10 drills file.

---
+++ ex10/drills_10.rb
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm \ttabbed in."
+persian_cat = "I'm split\fon a \vline"
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I\'ll do a list:
+\t* Cat \t\vfood
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
