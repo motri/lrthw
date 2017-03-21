77a559e9d320d31eace8c8c261e42f88045c4b5d
Unai Motriko <unaimotriko@gmail.com>
2017-03-21 17:06:07 +0000

    Exercise 13 first file

---
+++ ex13/01.rb
@@ -0,0 +1,5 @@
+first, second, third = ARGV # ARGV stands for argument variable
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
