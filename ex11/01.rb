7d83d9106af97090b8679004c2db0897bd0487a3
Unai Motriko <unaimotriko@gmail.com>
2017-03-21 17:26:02 +0000

    File modified to include .

--- ex11/01.rb
+++ ex11/01.rb
@@ -1,8 +1,8 @@
 print "How old are you? "
-age = gets.chomp
+age = $stdin.gets.chomp
 print "How tall are you? "
-height = gets.chomp
+height = $stdin.gets.chomp
 print "How much do you weight? "
-weight = gets.chomp
+weight = $stdin.gets.chomp
 
 puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
