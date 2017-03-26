f0e54e0c96e1ce33e01bad8fa493ffd42c324129
Unai Motriko <unaimotriko@gmail.com>
2017-03-26 16:24:53 +0100

    Exercise 34

---
+++ ex34/01.rb
@@ -0,0 +1,9 @@
+# animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+# the animal at 1 is the second and is ruby
+# the third animal is peacock and it is at 2
+# the first animal is bear and it is at 0
+# the animal at three is kangaroo and it is the fourth
+# the fifth animal is whale and it is at 4
+# the animal at 2 is peacock and it is the third
+# the sixth animal is platypus and it is at 5
+# the animal at 4 is whale and it is the fifth
