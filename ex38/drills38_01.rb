aa1c756c143fabab91a51f013957a98035060e13
Unai Motriko <unaimotriko@gmail.com>
2017-03-26 21:07:49 +0100

    Exercise 38 1st drill ammended

--- ex38/drills38_01.rb
+++ ex38/drills38_01.rb
@@ -2,13 +2,13 @@ ten_things = "Apples Oranges Crows Telephone Light Sugar"
 
 puts "Wait there are not 10 things in that list. Let's fix that."
 
-stuff = ten_things.split(' ') # split(ten_things, ' ')
+stuff = ten_things.split(' ') # split(ten_things, ' ') "Call split on ten_things with argument ' '"
 more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
 
 while stuff.length != 10
-  next_one = more_stuff.pop # pop(more_stuff)
+  next_one = more_stuff.pop # pop(more_stuff) "Call pop on more_stuff"
   puts "Adding: #{next_one}"
-  stuff.push(next_one) # push(stuff, next_one)
+  stuff.push(next_one) # push(stuff, next_one) "Call push on stuff with argument next_one"
   puts "There are #{stuff.length} items now."
 end
 
@@ -18,6 +18,6 @@ puts "Let's do some thing with stuff."
 
 puts stuff[1]
 puts stuff[-1]
-puts stuff.pop() # pop(stuff, )
-puts stuff.join(' ') # join(stuff, ' ')
-puts stuff[3..5].join("#") # join(stuff[3..5], '#')
+puts stuff.pop() # pop(stuff) "Call pop on stuff"
+puts stuff.join(' ') # join(stuff, ' ') "Call join on stuff with argument ' ' "
+puts stuff[3..5].join("#") # join(stuff[3..5], '#') "Call join on stuff indexes 3 to 5 with argument #"
