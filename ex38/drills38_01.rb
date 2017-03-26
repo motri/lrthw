c859754128ca7cbd33ae04104b82f30a1a2fdbcb
Unai Motriko <unaimotriko@gmail.com>
2017-03-26 21:02:40 +0100

    Exercise 38 1st drill

--- ex38/drills38_01.rb
+++ ex38/drills38_01.rb
@@ -2,13 +2,13 @@ ten_things = "Apples Oranges Crows Telephone Light Sugar"
 
 puts "Wait there are not 10 things in that list. Let's fix that."
 
-stuff = ten_things.split(' ')
+stuff = ten_things.split(' ') # split(ten_things, ' ')
 more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
 
 while stuff.length != 10
-  next_one = more_stuff.pop
+  next_one = more_stuff.pop # pop(more_stuff)
   puts "Adding: #{next_one}"
-  stuff.push(next_one)
+  stuff.push(next_one) # push(stuff, next_one)
   puts "There are #{stuff.length} items now."
 end
 
@@ -18,6 +18,6 @@ puts "Let's do some thing with stuff."
 
 puts stuff[1]
 puts stuff[-1]
-puts stuff.pop()
-puts stuff.join(' ')
-puts stuff[3..5].join("#")
+puts stuff.pop() # pop(stuff, )
+puts stuff.join(' ') # join(stuff, ' ')
+puts stuff[3..5].join("#") # join(stuff[3..5], '#')
